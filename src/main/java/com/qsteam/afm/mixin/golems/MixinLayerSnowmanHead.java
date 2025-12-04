03c245410ff6c3dd9cc1c5b62fdd1be9b8fda89b
Bronit <suslinv037@gmail.com>
2025-12-04 18:45:28 +0300

    snow golem model fix

---
+++ src/main/java/com/qsteam/afm/mixin/golems/MixinLayerSnowmanHead.java
@@ -0,0 +1,20 @@
+package com.qsteam.afm.mixin.golems;
+
+import com.qsteam.afm.block.AFMBlocks;
+import net.minecraft.client.renderer.entity.layers.LayerSnowmanHead;
+import net.minecraft.entity.monster.EntitySnowman;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(LayerSnowmanHead.class)
+public class MixinLayerSnowmanHead {
+
+    @Redirect(method = "doRenderLayer", at = @At(value = "NEW", target = "net/minecraft/item/ItemStack"))
+    private ItemStack useCarvedPumpkin(net.minecraft.block.Block block, int count) {
+        return new ItemStack(AFMBlocks.CARVED_PUMPKIN, 1);
+    }
+
+}
